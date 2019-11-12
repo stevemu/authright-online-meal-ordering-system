2689b527bd1392b78bea51c1e59bfb75897758ae
Jing L <jingl@Jings-MacBook-Pro.local>
2019-11-12 14:17:09 -0500

    Initial commit

--- client/src/pages/Home/Order.js
+++ client/src/pages/Home/Order.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 // presentational
 export default class Order extends React.Component {
-
+  
   render() {
-    return <div>order</div>
+    const orderItems = this.props.orderItems;
+    const items = [];
+
+    orderItems.forEach((element, index) => {
+      items.push(
+        <p key={element.itemId}>
+          <li>{element.name}</li>
+          <li>{element.quantity}</li>
+          <li>{element.price}</li>
+        </p>
+      );
+    });
+
+    return (
+      <div>
+        <h1>Order Summary</h1>
+        {items}
+      </div>
+    );
   }
 }
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -4,12 +4,44 @@ import Order from "./Order";
 
 // container
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orderItems: [
+        {
+          itemId: 'item1',
+          name: 'Roast Beef With Hot Chili Pepper Rolled In Scallion Pancake',
+          quantity: 2,
+          price: 9.50
+        },
+        {
+          itemId: 'item2',
+          name: 'Scallions Pancake',
+          quantity: 4,
+          price: 5.95,
+        },
+        {
+          itemId: 'item3',
+          name: 'Szechuan Wonton With Red Chilli Sauce',
+          quantity: 1,
+          price: 6.95,
+        },
+        {
+          itemId: 'item4',
+          name: 'Cucumber In Chili Sauce',
+          quantity: 2,
+          price: 7.50,
+        }
+      ]
+    };
+  }
 
   render() {
+    
     return (
       <div>
         <Menu />
-        <Order />
+        <Order orderItems={this.state.orderItems}/>
       </div>
     );
   }
