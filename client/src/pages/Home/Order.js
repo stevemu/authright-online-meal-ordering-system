9dbd85b2ca9704fdbe43a8dbc57189b90fe7ecd9
Jing L <jingl@Jings-MacBook-Pro.local>
2019-11-12 17:42:49 -0500

    added dropdown

--- client/src/pages/Home/Order.js
+++ client/src/pages/Home/Order.js
@@ -1,24 +1,33 @@
 import React from "react";
 
 import { DropdownButton, Dropdown } from "react-bootstrap";
+import GenerateDropdownItem from '../../components/GenerateDropdownItem';
 
 // presentational
 export default class Order extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSelectQuantityChange = this.handleSelectQuantityChange.bind(this);
+  }
 
   // A method to update the item price based on its quantity 
   updateItemPrice(price, quantity) {
     return (price*quantity).toFixed(2);
   }
 
-  handleChangeQuantity(e) {
-    console.log(e.target.value);
+  handleSelectQuantityChange(quantity, e, itemId) {
+    // sent back the updated quantity to API
+
+    // update the quantity in the data
+    // console.log(e.target.parentNode.parentNode.parentNode.parentNode.key);
+    this.props.handleOnSelectQuantity(quantity, itemId);
   }
 
   render() {
     const orderItems = this.props.orderItems;
     let subtotal = 0;
     const items = [];
-    let quantity = 0;
 
     orderItems.forEach((element) => {
       items.push(
@@ -27,8 +36,7 @@ export default class Order extends React.Component {
             <DropdownButton id="dropdown-basic-button" title={element.quantity} size="sm">
               <Dropdown.Item>Remove</Dropdown.Item>
               <Dropdown.Divider />
-              <Dropdown.Item onClick={(e) => this.handleChangeQuantity(e)}>1</Dropdown.Item>
-              
+              <GenerateDropdownItem id={element.itemId} onSelect={this.handleSelectQuantityChange} />
             </DropdownButton>
             {element.name} {"$"}{this.updateItemPrice(element.price, element.quantity)}
           </p>
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -9,39 +9,61 @@ class Home extends React.Component {
     this.state = {
       orderItems: [
         {
-          itemId: 'item1',
-          name: 'Roast Beef With Hot Chili Pepper Rolled In Scallion Pancake',
+          itemId: "item1",
+          name: "Roast Beef With Hot Chili Pepper Rolled In Scallion Pancake",
           quantity: 2,
-          price: 9.50
+          price: 9.5
         },
         {
-          itemId: 'item2',
-          name: 'Scallions Pancake',
+          itemId: "item2",
+          name: "Scallions Pancake",
           quantity: 4,
-          price: 5.95,
+          price: 5.95
         },
         {
-          itemId: 'item3',
-          name: 'Szechuan Wonton With Red Chilli Sauce',
+          itemId: "item3",
+          name: "Szechuan Wonton With Red Chilli Sauce",
           quantity: 1,
-          price: 6.95,
+          price: 6.95
         },
         {
-          itemId: 'item4',
-          name: 'Cucumber In Chili Sauce',
+          itemId: "item4",
+          name: "Cucumber In Chili Sauce",
           quantity: 2,
-          price: 7.50,
+          price: 7.5
         }
       ]
     };
+
+    this.handleQuantity = this.handleQuantity.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  // update order item quantity
+  handleQuantity(quantity, itemId) {
+    this.setState({
+      orderItems: this.state.orderItems.map(element => 
+        element.itemId === itemId ? {...element, quantity: quantity} : element
+      )
+    });
+  }
+
+  handleDelete(itemId) {
+    // this.setState({
+    //   orderItems: this.state.orderItems.filter((element) => element.itemId !== itemId)
+    // });
+    console.log('Delete!', itemId);
   }
 
   render() {
-    
     return (
       <div>
         <Menu />
-        <Order orderItems={this.state.orderItems}/>
+        <Order
+          orderItems={this.state.orderItems}
+          handleOnSelectQuantity={this.handleQuantity}
+          handleOnDelete={this.handleDelete}
+        />
       </div>
     );
   }
