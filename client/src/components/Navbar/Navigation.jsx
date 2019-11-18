9f8559c9de09f5f02fbb5ad1edb9421f140cc6a8
ar-hathaway <xinyi.zhang@authright.com>
2019-11-18 16:36:48 -0500

    Adding input box style

--- client/src/components/Navbar/Navigation.jsx
+++ client/src/components/Navbar/Navigation.jsx
@@ -9,18 +9,7 @@ const Navigation = (props) => {
             <Navbar bg="light" variant="light" fixed="top" style={styles.navigation}>
                 <Navbar.Brand href="/">Order Food</Navbar.Brand>
                 <Nav className="mr-auto" />
-                {/* <form onSubmit={props.handleChange}> */}
-                    {/* <Form inline role="form"> */}
-                    <input type="text" placeholder="Search" onChange={props.handleChange}/>
-                    {/* <button type="submit">Search</button> */}
-                    {/* <FormControl type="text" placeholder="Search" />
-                        <Button variant="outline-primary" type="submit">Search</Button> */}
-                    {/* </Form> */}
-                {/* </form> */}
-                {/* <Form inline onSubmit={props.handleChange}>
-                    <FormControl type="text" placeholder="Search" />
-                    <Button variant="outline-primary">Search</Button>
-                </Form> */}
+                <input type="text" placeholder="Search" onChange={props.handleChange} style={styles.input}/>
             </Navbar>
         </div>
     )
